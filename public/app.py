6773d2c563bf0ae809638fbaa11bfc12676e9eee
jebc314 <jebcui99@gmail.com>
2022-04-17 01:09:44 -0400

    Fix HTTPs calls

--- public/app.py
+++ public/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template,request
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,4 +8,8 @@ def index():
 
 @app.route('/puzzle', methods=["GET"])
 def puzzle():
-    return "puzzle output"
+    output = {
+        'difficulty':request.args.get('difficulty'),
+        'piecescount':request.args.get('piecescount')
+    }
+    return json.dumps(output)
--- public/static/scripts/index.js
+++ public/static/scripts/index.js
@@ -2,12 +2,14 @@ function call_puzzle(data) {
     console.log(data);
     const Http = new XMLHttpRequest();
        
-    const url = '/puzzle?data' + data; 
+    const url = '/puzzle?difficulty='+data['difficulty']+'&piecescount='+data['piecescount']; 
     Http.open("GET", url);
     Http.send();
 
     Http.onreadystatechange = (e) => {
-        output = Http.responseText;//JSON.parse(Http.responseText);
-        console.log(output);
+        if (Http.readyState == 4 && Http.status == 200) {
+            output = JSON.parse(Http.responseText);
+            console.log(output);
+        }
     }
 }
